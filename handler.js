af2696b2ac25165cfc497869ba3a929b5ab515e5
Nayee22 <140297710+Nayee22@users.noreply.github.com>
2023-08-22 17:35:50 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -986,7 +986,7 @@ export async function handler(chatUpdate) {
         if (!('autoread2' in settings)) settings.autoread2 = false;
         if (!('restrict' in settings)) settings.restrict = false;
         if (!('antiCall' in settings)) settings.antiCall = false;
-        if (!('antiPrivate' in settings)) settings.antiPrivate = false;
+        if (!('antiPrivate' in settings)) settings.antiPrivate = true;
 	if (!('modejadibot' in settings)) settings.modejadibot = true;
         if (!('antispam' in settings)) settings.antispam = false;
 	if (!('audios_bot' in settings)) settings.audios_bot = true;      
@@ -997,7 +997,7 @@ export async function handler(chatUpdate) {
           autoread2: false,
           restrict: false,
           antiCall: false,
-          antiPrivate: false,
+          antiPrivate: true,
 	  modejadibot: true,
           antispam: false,
 	  audios_bot: true	
