4b9a90bd40c80c7ba3d6d68224351b9fb8e0f26f
Nayee22 <140297710+Nayee22@users.noreply.github.com>
2023-08-22 19:23:24 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,21 +1,21 @@
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
-  if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
-  }
-  const pesan = args.join` `;
-  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
-  for (const mem of participants) {
-    teks += `┣➥ @${mem.id.split('@')[0]}\n`;
-  }
-  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
-};
-handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
-handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
-handler.admin = true;
-handler.group = true;
-export default handler;
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+let vn = './media/Invocar.mp3'
+let pesan = args.join` `
+let oi = `*By : Ezequiel* ${pesan}`
+let teks = `*revivan y compren , necesito comer*\n\n ${oi}\n\n🫶🏻 ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂\n`
+for (let mem of participants) {
+teks += `💦  @${mem.id.split('@')[0]}\n`}
+teks += `*💌Bot de Ezequiel*\n\n*<3*`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
+}
+handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.tags = ['group']
+handler.command = /^(tagall|invocar|todas|todos|todes|fantasmas|adornos|plantas)$/i
+handler.admin = true
+handler.group = true
+export default handler
